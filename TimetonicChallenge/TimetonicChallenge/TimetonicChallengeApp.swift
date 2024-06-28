e43e8a785d99d82e2f6cf0e01b3a15f9c03f74a6
Tatiana Carvajal <taticv159912@gmail.com>
2024-06-28 09:32:06 -0500

    First commit

---
+++ TimetonicChallenge/TimetonicChallenge/TimetonicChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimetonicChallengeApp.swift
+//  TimetonicChallenge
+//
+//  Created by Tatiana Carvajal on 28/06/24.
+//
+
+import SwiftUI
+
+@main
+struct TimetonicChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
